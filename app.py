d9b6b42221be3f3149ff0ce8939ce6299102d35e
Semen <semenggde@gmail.com>
2022-05-05 18:31:53 +0200

    Celerary UDP APP

--- app.py
+++ app.py
@@ -1,13 +1,10 @@
 from flask import Flask, render_template, Response
 import cv2
-from picamera import PiCamera
-from time import sleep
-
 
 app = Flask(__name__)
-camera = cv2.VideoCapture(0)
+camera = cv2.VideoCapture(1)
 
-"""
+
 def generate_frames():
     while True:
 
@@ -21,24 +18,17 @@ def generate_frames():
 
         yield (b'--frame\r\n'
                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
-"""
-def get_picture():
-        camera = PiCamera()
-        camera.start_preview()
-        sleep(5)
-        camera.capture('picture.jpg')
-        camera.stop_preview()
 
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-"""
+
 @app.route('/video')
 def video():
     return Response(generate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
-"""
+
 
 if __name__ == "__main__":
     try:
