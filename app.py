0cd03b71b4fc5284b6cb4cc834946faf335ce5db
Kirsc <semenggde@gmail.com>
2022-05-06 12:52:55 +0100

    cam

--- app.py
+++ app.py
@@ -24,8 +24,6 @@ def generate_frames():
 """
 def get_picture():
         camera = PiCamera()
-        camera.start_preview()
-        sleep(5)
         camera.capture('picture.jpg')
         camera.stop_preview()
 
