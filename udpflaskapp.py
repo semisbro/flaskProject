78722047a5a7e47a836ead2594e3dd0bf6dcf511
Semen <semenggde@gmail.com>
2022-05-08 14:54:56 +0200

    celery UDP APP + other cam

--- udpflaskapp.py
+++ udpflaskapp.py
@@ -32,19 +32,79 @@ def listen_to_udp():
     """
     This code was taken from
     https://stackoverflow.com/questions/9969259/python-raw-socket-listening-for-udp-packets-only-half-of-the-packets-received
-    """
-    print("task is running")
 
-    udp_socket: socket.socket
-    udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    udp_socket.bind(('0.0.0.0', 1337))
+
+    """
+    import json
+    import socket
+    import dronekit
+    import dronekit_sitl
+    from dronekit import connect
+    import dronekit_sitl
+
+    sitl = dronekit_sitl.start_default()
+    connection_string = sitl.connection_string()
+
+    # Import DroneKit-Python
+    from dronekit import connect, VehicleMode
+
+    print("Start simulator (SITL)")
+
+    localIP = "192.168.0.104"
+    localPort = 20002
+    bufferSize = 1024
+
+    vehicle = connect(connection_string, wait_ready=True)
+
+    # Create a datagram socket
+
+    UDPServerSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+
+    # Bind to address and ip
+
+    UDPServerSocket.bind((localIP, localPort))
+
+    print("UDP server up and listening")
+
+    # Listen for incoming datagrams
+
+    while True:
+        bytesAddressPair = UDPServerSocket.recvfrom(bufferSize)
+
+        latitude = vehicle.location.global_relative_frame.lat
+        longitude = vehicle.location.global_relative_frame.lon
+        print(latitude)
+        print(longitude)
+        map_dat = dict(latitude=latitude, longitude=longitude)
+
+        message = bytesAddressPair[0]
+
+        address = bytesAddressPair[1]
+
+        clientMsg = "Message from Client:{}".format(message)
+        clientIP = "Client IP Address:{}".format(address)
+
+        # print(clientMsg)
+        # print(clientIP)
+        msgFromServer = 'Empty'
+
+        bytesToSend: bytes = str.encode(json.dumps(map_dat))
+
+        # Sending a reply to client
+
+        UDPServerSocket.sendto(bytesToSend, address)
+    #print("task is running")
+
+    #udp_socket: socket.socket
+    ##udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    #udp_socket.bind(('0.0.0.0', 1337))
     #  s2 = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_UDP)
     #  s2.bind(('0.0.0.0', 1337))
-    print("task is running")
-    while True:
-        r, w, x = select.select([udp_socket], [], [])
-        for i in r:
-            print((i, i.recvfrom(131072)))
+    #print("task is running")
+    #while True:
+      #  r, w, x = select.select([udp_socket], [], [])
+     #   for i in r:
+       #     print((i, i.recvfrom(131072)))
 
 
 @app.route("/")
