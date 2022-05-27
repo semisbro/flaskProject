a355a37a3a97e2cfe9471bc97142c2e4c64f372f
Semen <semenggde@gmail.com>
2022-05-27 18:25:52 +0200

    MAVSDK

--- mavsdk_test.py
+++ mavsdk_test.py
@@ -1,6 +1,21 @@
+#!/usr/bin/env python3
+import asyncio
 from mavsdk import System
-
-...
-
-drone = System()
-await drone.connect()
+async def run():
+    drone = System()
+    await drone.connect()
+    print("Waiting for drone...")
+    async for state in drone.core.connection_state():
+        if state.is_connected:
+            print(f"Drone discovered with UUID: {state.uuid}")
+            break
+    print("-- Arming")
+    await drone.action.arm()
+    print("-- Taking off")
+    await drone.action.takeoff()
+    await asyncio.sleep(5)
+    print("-- Landing")
+    await drone.action.land()
+if __name__ == "__main__":
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(run())
