d8b7de978aaddc67ff7b4f295715c39a8cc7903b
Kirsc <semenggde@gmail.com>
2022-05-27 13:45:18 +0100

    arm check

--- arm_check.py
+++ arm_check.py
@@ -2,7 +2,7 @@ from datetime import time
 
 import dronekit
 
-vehicle = dronekit.connect('/dev/ttyAMA0', wait_ready=True)
+vehicle = dronekit.connect('tcp:127.0.0.1:5760', wait_ready=True)
 
 def arm_and_takeoff(aTargetAltitude):
     """
