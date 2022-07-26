c9eeb8ab31f5a6f22c8ead54622304dc1f3e5074
Semen <semenggde@gmail.com>
2022-07-26 13:03:13 +0200

    ALT video stream

--- udpflaskapp.py
+++ udpflaskapp.py
@@ -10,7 +10,6 @@ import psutil
 
 import cv2
 from imutils.video import VideoStream
-from pymavlink import mavutil
 
 
 def make_celery(app):
@@ -183,25 +182,6 @@ def test_home():
     return jsonify(d)
 
 
-def send_ned_velocity(vehicle,velocity_x, velocity_y, velocity_z, duration):
-    """
-    Move vehicle in direction based on specified velocity vectors.
-    """
-    msg = vehicle.message_factory.set_position_target_local_ned_encode(
-        0,       # time_boot_ms (not used)
-        0, 0,    # target system, target component
-        mavutil.mavlink.MAV_FRAME_LOCAL_NED, # frame
-        0b0000111111000111, # type_mask (only speeds enabled)
-        0, 0, 0, # x, y, z positions (not used)
-        velocity_x, velocity_y, velocity_z, # x, y, z velocity in m/s
-        0, 0, 0, # x, y, z acceleration (not supported yet, ignored in GCS_Mavlink)
-        0, 0)    # yaw, yaw_rate (not supported yet, ignored in GCS_Mavlink)
-
-
-    # send command to vehicle on 1 Hz cycle
-    for x in range(0,duration):
-        vehicle.send_mavlink(msg)
-        time.sleep(1)
 
 if __name__ == "__main__":
     try:
