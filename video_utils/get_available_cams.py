b3062a4089919bca50f93d456b25f0b3ba8a1e12
Semen <semenggde@gmail.com>
2022-07-30 11:58:31 +0200

    video utils

--- video_utils/get_available_cams.py
+++ video_utils/get_available_cams.py
@@ -1,15 +1,26 @@
 import cv2
+import cv2
 
-index = 0
-arr = []
-while True:
-    cap = cv2.VideoCapture(index)
-    try:
-        if cap.getBackendName()=="MSMF":
-            arr.append(index)
-    except:
-        break
-    cap.release()
-    index += 1
+def list_ports():
 
-print(arr)
+    non_working_ports = []
+    dev_port = 0
+    working_ports = []
+    available_ports = []
+    while len(non_working_ports): # if there are more than 5 non working ports stop the testing.
+        camera = cv2.VideoCapture(dev_port)
+        if not camera.isOpened():
+            non_working_ports.append(dev_port)
+            print("Port %s is not working" %dev_port)
+        else:
+            is_reading, img = camera.read()
+            w = camera.get(3)
+            h = camera.get(4)
+            if is_reading:
+                print("&quot;Port %s is working and reads images (%s x %s)" %(dev_port,h,w))
+                working_ports.append(dev_port)
+            else:
+                print("Port %s for camera ( %s x %s) is present but does not reads." %(dev_port,h,w))
+                available_ports.append(dev_port)
+        dev_port +=1
+    return available_ports,working_ports,non_working_ports
