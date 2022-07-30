e1805407c0f89749cdc6ea9f8cca2c1c3799a558
Semen <semenggde@gmail.com>
2022-07-30 12:40:06 +0200

    video utils

--- video_utils/get_available_cams.py
+++ video_utils/get_available_cams.py
@@ -1,7 +1,7 @@
 import cv2
 
 
-def list_ports():
+def get_available_camera_port() :
     """
     Test the ports and returns a tuple with the available ports and the ones that are working.
     """
@@ -9,23 +9,24 @@ def list_ports():
     dev_port = 0
     working_ports = []
     available_ports = []
-    while len(non_working_ports) < 60: # if there are more than 5 non working ports stop the testing.
+    while len(non_working_ports) < 20:  # if there are more than 5 non working ports stop the testing.
         camera = cv2.VideoCapture(dev_port)
         if not camera.isOpened():
             non_working_ports.append(dev_port)
-            print("Port %s is not working." %dev_port)
+            print("Port %s is not working." % dev_port)
         else:
             is_reading, img = camera.read()
             w = camera.get(3)
             h = camera.get(4)
             if is_reading:
-                print("Port %s is working and reads images (%s x %s)" %(dev_port,h,w))
+                print("Port %s is working and reads images (%s x %s)" % (dev_port, h, w))
                 working_ports.append(dev_port)
             else:
-                print("Port %s for camera ( %s x %s) is present but does not reads." %(dev_port,h,w))
+                print("Port %s for camera ( %s x %s) is present but does not reads." % (dev_port, h, w))
                 available_ports.append(dev_port)
-        dev_port +=1
-    return available_ports,working_ports,non_working_ports
+        dev_port += 1
+    if not working_ports:
+        raise Exception("No Cameras attached, please  ensure that one camera is working")
+    return working_ports[0]
 
 
-print(list_ports())
